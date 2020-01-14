242992169047774bf5738db595100df93f647234
jidibinlin <2694273649@qq.com>
2020-01-14 18:34:40 +0800

    remove bar

--- autostart.sh
+++ autostart.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
+
+dwmbar &
 setxkbmap -option ctrl:swapcaps &
 light-locker &
 xfce4-power-manager &
-lantern &
 
 picom -b &
 
 feh --bg-fill --randomize '/home/qibin/Pictures/Wallpaper/' &
 xinput set-prop --type=int --format=8 "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1 &
-
-/bin/bash ~/.config/autostart/dwm-bar/dwm_bar.sh &
-
+lantern &
